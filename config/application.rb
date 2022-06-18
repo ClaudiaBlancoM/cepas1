78291ea29957d931f5f0cbd618c7ce73d17b98b1
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-17 23:22:24 -0400

    Feat:adding languages to project

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module Cepas1
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # available languages 
+    config.i18n.available_locales = [:en, :es]
+
+    # lenguage by default 
   end
 end
