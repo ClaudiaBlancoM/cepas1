16d1ca89a291d91d02d7a9da36ab1fa9f3f83a0a
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-18 17:41:52 -0400

    feat: configurating devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   resources :strains
   resources :wines
-  root "strains#index"
   # root "strains#index"
   root to: "home#index"
 end
