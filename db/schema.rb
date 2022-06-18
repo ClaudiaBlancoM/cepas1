b5b8cf1594482752a2b14755b0155bcd9bc5e914
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-17 22:47:00 -0400

    feat:Wine MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_024324) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_18_024622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "strains", force: :cascade do |t|
     t.string "name"
     t.boolean "avalaible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wines", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
